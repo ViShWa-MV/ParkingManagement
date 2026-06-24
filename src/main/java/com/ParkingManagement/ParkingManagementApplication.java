bf28bd1713de06decd89510c733bb3cb182b414e
ViShWa-MV <mvishwa270@gmail.com>
2026-06-24 11:30:55 +0530

    Extend ParkingManagementApplication for servlet support

--- src/main/java/com/ParkingManagement/ParkingManagementApplication.java
+++ src/main/java/com/ParkingManagement/ParkingManagementApplication.java
@@ -2,12 +2,18 @@ package com.ParkingManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ParkingManagementApplication {
+public class ParkingManagementApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingManagementApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ParkingManagementApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingManagementApplication.class, args);
+    }
 }
